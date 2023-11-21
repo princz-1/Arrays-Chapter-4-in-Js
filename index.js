a2af7d0501b7a1721ca993d37fee1c44314828cc
princz-1 <150322497+princz-1@users.noreply.github.com>
2023-11-21 04:52:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+/* Arrays in JavaScript 
+- Collections of items 
+
+-- Strings --> immutable
+-- Arrays --> mutable
+*/
+
+// Create arrays
+let marks = [24,34,45,56,67,78];
+console.log(marks)
+console.log(marks.length)//property
+
+let heroes = ["ironman","hulk","antman","shaktiman","thor","batman"]
+console.log(heroes)
+console.log(heroes.length)
+
+let info = ["Prince",48,"delhi"]
+console.log(info)
+console.log(info.length)
+
+// Arrays indices
+let Marks = [23,87,45,67,78,32,89,09]
+
+console.log(Marks[0],Marks[5],Marks[8])
+
+console.log(Marks[0] = 87)
+
